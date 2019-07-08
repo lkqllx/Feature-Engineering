f40143a2149f790c7dbe5fc6366419ae918b47ff
andrew_alien <lkqllx@gmail.com>
2019-07-08 22:21:28 +0800

    pull from origin

--- FeatureEng.py
+++ FeatureEng.py
@@ -15,8 +15,22 @@ from keras.layers.recurrent import LSTM
 import os
 import h5py
 import datetime as dt
+from statsmodels.api import OLS, add_constant
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+
+
+def parse_date(date):
+    return dt.datetime.strptime(date, '%Y-%m-%d')
+
+def next_weekday(date):
+    date += dt.timedelta(days=1)
+    for _ in range(7):
+        if date.weekday() < 5:
+            break
+        else:
+            date += dt.timedelta(days=1)
+    return date
 
 def concat_data(path='data/'):
     files = os.listdir(path)
@@ -33,7 +47,7 @@ def concat_data(path='data/'):
         if idx % 5 == 0:
             print(f'Processing {idx}')
 
-def comp_BM(file='data/step1_0050.csv'):
+def comp_BM(file='data/step1_2492.csv'):
     try:
         curr_df = pd.read_csv(file, index_col=0)
     except:
@@ -49,9 +63,62 @@ def comp_BM(file='data/step1_0050.csv'):
         BM[col] = round(correct / total, 2)
     print(BM)
 
+def split_dataset(path='data/'):
+    files = os.listdir(path)
+    for file in files:
+        start = '2018-01-22'
+        if file.split('.')[1] == 'csv':
+            df = pd.read_csv(path+file, index_col=0)
+            df['date_str'] = df['date'].values
+            df.date = df['date'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d'))
+            training = fetch_training(df, training_start='2018-01-22', duration=14)
+            testing = fetch_testing(df, testing_start='2018-01-22', duration=1)
+            df.drop('date_str', axis=1)
+
+
+def fetch_training(df, training_start, duration):
+    """
+    Fetch the training data for a fixed period
+    :return: output - in that duration
+    """
+    if not dt.datetime.strptime(training_start, '%Y-%m-%d').weekday() < 5:
+        print('Start date should be weekday!')
+        return False
+
+    dt_training_start = dt.datetime.strptime(training_start, '%Y-%m-%d')
+    dt_training_end = dt_training_start + dt.timedelta(days=duration)
+    try:
+        return df.loc[(df.date < dt_training_end) & (df.date >= dt_training_start)]
+
+    except:
+        print('Specific duration is not contained in the dataframe!')
+        return False
+
+def fetch_testing(df, testing_start, duration=1):
+    """
+    Fetch the testing data for a fixed period but the testing start may not in the dataframe
+    So we need to look for the next available start date
+    :return: output - in that duration
+    """
+    dt_testing_start = dt.datetime.strptime(testing_start, '%Y-%m-%d')
+    date_set = set(df['date_str'].values.tolist())
+    for _ in range(200):
+        if testing_start in date_set:
+            dt_testing_end = dt_testing_start + dt.timedelta(days=duration)
+            break
+        else:
+            dt_testing_start = next_weekday(dt_testing_start)
+    try:
+        return df.loc[(df.date < dt_testing_end) & (df.date >= dt_testing_start)]
+
+    except:
+        print('Specific duration is not contained in the dataframe!')
+        return False
+
+
 
 class Preprocess:
-    def preprocess(self, df: pd.DataFrame, threshold: float = 0.0001, time_step=5) -> (pd.DataFrame, pd.DataFrame):
+    def preprocess(self, df: pd.DataFrame, threshold: float = 0.0001, time_step=5, num_feature=8, normalize=True) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
         label = pd.DataFrame(index=df.index)
         for idx, name in enumerate(df.columns):
             "Creat training labels"
@@ -61,7 +128,7 @@ class Preprocess:
                 label.loc[df[name] < -threshold, 'Label_{}'.format(idx)] = 2
                 label.loc[abs(df[name]) <= threshold, 'Label_{}'.format(idx)] = 0
 
-            if int(idx) >= 8:
+            if int(idx) >= num_feature and normalize:
                 "Normalize the dataframe"
                 df.loc[:, name] = (df[name] - df[name].mean()) / (df[name].max() - df[name].min())
 
@@ -71,13 +138,18 @@ class Preprocess:
 
 
         y_reg = df.iloc[:, 2:7]
-        df = df.iloc[:, 8:]
+        df = df.iloc[:, num_feature:]
         for idx in range(0, 10 - 1):
-            if (df.size - 55 * idx) % (time_step * 55) == 0:
+            if (df.size - (df.shape[1]) * idx) % (time_step * df.shape[1]) == 0:
                 possible_reshape_row = idx
                 break
 
         return df[possible_reshape_row:], label[possible_reshape_row::time_step], y_reg[possible_reshape_row::time_step]
+
+
+
+
+
 
 class NeuralNet(Preprocess):
     def __init__(self, df, time_step = 5):
@@ -96,22 +168,24 @@ class NeuralNet(Preprocess):
 
     def run(self):
         self.processed_df, self.label, _ = self.preprocess(self.df, 0.0001)
-        nn = self.create_network()
-        nn.fit(self.processed_df.values.reshape((-1, self.time_step, 55)),
+        self.nn = self.create_network()
+        self.nn.fit(self.processed_df.values.reshape((-1, self.time_step, 55)),
                 np_utils.to_categorical(self.label['Label_1'], num_classes=3),
-                epochs=20, validation_split=0.1)
+                epochs=20, validation_split=0.05, shuffle=False)
+
+    def predict(self):
+        self.nn.predict()
+
 
 class Regressor(Preprocess):
-    """
-    TODO: Handle weekend
-    """
-    def __init__(self, df, train_interval=10, test_interval=1, test_period=100):
+    def __init__(self, df, train_interval=14, test_interval=1, test_period=100, num_feature=16):
         """
         Regressor for linear regression
         :param df: input data
         :param train_interval: length of training period
         :param test_period: how many days to be tested
         """
+        super().__init__()
         self.df = df
         self.train_interval = train_interval
         self.test_period = test_period
@@ -119,13 +193,19 @@ class Regressor(Preprocess):
         self.curr_start = df.date[0]
         self.date = df.date.reset_index(drop=True)
         self.curr_pointer = 0 # pointed to the index of current date
-        self.preprocess_df, _, self.label = self.preprocess(df=df, time_step=1)
+        self.preprocess_df, _, self.label = self.preprocess(df=df, time_step=1, num_feature=num_feature, normalize=False)
 
 
     def fetch_data(self):
+        """
+        Fetch data for a fixed duration.
+
+        If train_interval = 14, test_interval = 1
+        The function will fetch the training data of next two weeks' available data regardless the weekends
+        :return: training and testing data
+        """
         train_start = dt.datetime.strptime(self.curr_start, '%Y-%m-%d')
         train_end = train_start + dt.timedelta(days=self.train_interval)
-        test_end = train_end + dt.timedelta(days=self.test_interval)
 
         train_start_idx = self.curr_pointer
         updated = False
@@ -138,49 +218,59 @@ class Regressor(Preprocess):
             if (train_end - dt.datetime.strptime(self.date[train_idx], '%Y-%m-%d')).days <= -1:
                 train_end_idx = train_idx - 1
                 test_start_idx = train_idx
+                test_end = dt.datetime.strptime(self.date[test_start_idx], '%Y-%m-%d') # only one day test
                 break
 
         for test_idx in range(test_start_idx, self.date.shape[0]):
             if (test_end - dt.datetime.strptime(self.date[test_idx], '%Y-%m-%d')).days <= -1:
                 test_end_idx = test_idx - 1
                 break
-
-
-        # print(f'Train start  - {self.date[train_start_idx]}')
-        # print(f'Train end  - {self.date[train_end_idx]}')
-        # print(f'Test start  - {self.date[test_start_idx]}')
-        # print(f'Test end  - {self.date[test_end_idx]}')
+        print(f'Train start  - {self.date[train_start_idx]}')
+        print(f'Train end  - {self.date[train_end_idx]}')
+        print(f'Test start  - {self.date[test_start_idx]}')
+        print(f'Test end  - {self.date[test_end_idx+1]}')
 
         return self.preprocess_df[train_start_idx:train_end_idx], self.label[train_start_idx:train_end_idx],\
                self.preprocess_df[test_start_idx:test_end_idx], self.label[test_start_idx:test_end_idx]
 
+    def run_regr(self):
+        preprocess_df = add_constant(self.preprocess_df)
+        regr = OLS(self.label['Y_M_1'], preprocess_df).fit()
+        print(regr.summary())
+        for _ in range(50):
+            try:
+                train_x, train_y, test_x, test_y = self.fetch_data()
+                # regr = OLS(train_y['Y_M_1'], train_x).fit()
+                # print(regr.summary())
+                # y_pred = regr.predict(test_x)
+                # # print(f'Mean error is {mean_squared_error(test_y, y_pred)}')
+                # print(f'R-square is {r2_score(test_y, y_pred)}')
+            except:
+                pass
 if __name__ == '__main__':
-    comp_BM(file='data/step1_0050.csv')
+    # comp_BM(file='data/step1_1101.csv')
     # concat_data()
 
     """
     Preprocess dataframe and reshape the data fed into neural networks
     """
-    df = pd.read_csv('data/step1_0050.csv', index_col=0).dropna()
-    nn = NeuralNet(df, time_step=5)
-    nn.run()
+    # df = pd.read_csv('data/step1_0050.csv', index_col=0).dropna()
+    # nn = NeuralNet(df, time_step=30)
+    # nn.run()
 
 
     """
     1.Preprocess dataframe regarding to different time scale
     2.Build the regression model
     """
-    # df = pd.read_csv('data/step1_0050.csv', index_col=0).dropna()
-    # reg = Regressor(df=df)
-    # for _ in range(100):
-    #     try:
-    #         train_x, train_y, test_x, test_y = reg.fetch_data()
-    #         regr = LinearRegression().fit(train_x, train_y)
-    #         y_pred = regr.predict(test_x)
-    #         # print(f'Mean error is {mean_squared_error(test_y, y_pred)}')
-    #         print(f'R-square is {r2_score(test_y, y_pred)}')
-    #     except:
-    #         pass
+    df = pd.read_csv('data/step1_0050.csv', index_col=0).dropna()
+    df['date_str'] = df['date'].values
+    df.date = df['date'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d'))
+    # training = fetch_training(df, training_start='2018-01-22', duration=14)
+    testing = fetch_testing(df, testing_start='2018-01-22', duration=1)
+    df.drop('date_str', axis=1)
+    reg = Regressor(df=df)
+
 
 
 
